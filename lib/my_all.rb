68ce553821954449c584647e0e442b37ee7833fa
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-08-20 19:40:25 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_all?(collection)
   count = 0
   while count < collection.length
-    yield(collection[count])
+    block_return << yield(collection[count])
     count += 1 
   end
 end
